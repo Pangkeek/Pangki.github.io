889c480f216a68377f8905afeddf0219a992d1ba
Pangkeek <supawatkhaithong@gmail.com>
2023-09-19 21:27:43 +0700

    animation slide

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) =>{
         console.log(entry)
         if (entry.isIntersecting) {
-            entry.target.classList.ass("show")
+            entry.target.classList.add("show")
         }else {
             entry.target.classList.remove("show")
         }
     })
 })
 
-const hiddenE = document.querySelectorAll('hidden')
+const hiddenE = document.querySelectorAll('.hidden')
 hiddenE.forEach((el) => observer.observe(el))
