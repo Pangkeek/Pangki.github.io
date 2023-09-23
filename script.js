8211bafb86f901f6be82e52619b4bb8e823c5f86
Pangkeek <supawatkhaithong@gmail.com>
2023-09-24 01:02:33 +0700

    navbar

--- script.js
+++ script.js
@@ -11,3 +11,48 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenE = document.querySelectorAll('.hidden')
 hiddenE.forEach((el) => observer.observe(el))
+
+
+
+const scroll1Elements1 = document.querySelectorAll(".click1");
+scroll1Elements1.forEach(element => {
+  element.addEventListener("click", () => {
+    window.scrollTo({
+      top: 1,
+      behavior: "smooth"
+    });
+  });
+});
+
+
+const scroll1Elements2 = document.querySelectorAll(".click2");
+scroll1Elements2.forEach(element => {
+  element.addEventListener("click", () => {
+    window.scrollTo({
+      top: 650,
+      behavior: "smooth"
+    });
+  });
+});
+
+
+const scroll1Elements3 = document.querySelectorAll(".click3");
+scroll1Elements3.forEach(element => {
+  element.addEventListener("click", () => {
+    window.scrollTo({
+      top: 1290,
+      behavior: "smooth"
+    });
+  });
+});
+
+
+const scroll1Elements4 = document.querySelectorAll(".click4");
+scroll1Elements4.forEach(element => {
+  element.addEventListener("click", () => {
+    window.scrollTo({
+      top: 50000,
+      behavior: "smooth"
+    });
+  });
+});
